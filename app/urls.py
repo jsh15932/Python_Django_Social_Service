46c6a84c75056621c6225cad19c0591e2c3b4b10
장승훈 <jsh1400512@naver.com>
2020-05-22 04:19:36 +0900

    Commit

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import app_detail_view, app_list_view
+from .views import app_detail_view, app_list_view, AppListView, AppDetailView
 
 urlpatterns = [
     url(r'^$', app_list_view, name='list'),
--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
 from .models import App
+
+class AppDetailView(DetailView):
+    # template_name = "app/detail_view.html"
+    queryset = App.objects.all()
+
+    def get_object(self):
+        return App.objects.get(id=1)
+
+class AppListView(ListView):
+    # template_name = "app/list_view.html"
+    queryset = App.objects.all()
+
 
 def app_detail_view(request, id=1):
     obj = App.objects.get(id=id)
@@ -8,7 +21,7 @@ def app_detail_view(request, id=1):
     context = {
         "object" : obj
     }
-    return render(request, "app/detail_view.html", context)
+    return render(request, "app/app_detail.html", context)
 
 def app_list_view(request, id=1):
     queryset = App.objects.all()
@@ -18,4 +31,4 @@ def app_list_view(request, id=1):
     context = {
         "object_list" : queryset
     }
-    return render(request, "app/list_view.html", context)
+    return render(request, "app/app_list.html", context)
