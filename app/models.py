51be53324c634bbd7a8f83e36fa380beb4e14d81
장승훈 <jsh1400512@naver.com>
2020-05-27 16:57:08 +0900

    Commit

--- app/models.py
+++ app/models.py
@@ -4,14 +4,6 @@ from django.conf import settings
 from django.views.generic import DetailView, ListView, CreateView
 from .forms import AppModelForm
 
-class App(models.Model):
-    content = models.CharField(max_length = 140)
-    timestamp = models.DateTimeField(auto_now = True)
-    updated = models.DateTimeField(auto_now_add = True)
-    
-    def __str__(self):
-        return str(self.content)
-
 class AppCreateView(CreateView):
     form_class = AppModelForm
     template_name = 'app/create_view.html'
